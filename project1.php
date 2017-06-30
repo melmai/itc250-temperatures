3ff9f4213b850e53d8abbdbae51417ebccd4d861
Anu Slorah <anuslorah@users.noreply.github.com>
2017-06-30 10:27:58 -0700

    Add files via upload
    Number validation done, needs formulas from other members and we still need:
    Visual Elements Required: Your program design document should identify an IPO chart and a flowchart of how the application will operate and process data.
    Since I built the form, I'd appreciate if someone else would draw the flow chart. Source code is here and also on google doc 
    https://docs.google.com/document/d/15B6fVg73fIGLtcdH4gWoM4R4JMHp7ct7Yx7nYd_2NQw/edit#

--- project1.php
+++ project1.php
@@ -3,7 +3,12 @@
     <style>
         h1 {
             text-align: center;
+            padding-top: 25px
             padding: 16p;
+        }
+        h3 {
+            text-align: center;
+            padding: inherit;
         }
         table {
             margin-left:auto;
@@ -11,47 +16,17 @@
             padding: 16px;
         }
         td {
-            padding: 6px;
-        }
-        #button {
-            text-align: center;
-        }
-        #output {
-            border: 1px solid black;
+            padding-left: 6px;
+            padding-right: 6px;
         }
     </style>
 </head>
-<body>
-    <h1>Temperature Converter</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-      <table>    
-            <tr>
-                <td><h3>Convert from:</h3></td>
-                <td><input type="text" name="input" size="6"></td>
-                <td><input type="radio" name="DegreesIn" value="Celsius" checked="checked">Celsius<br>
-                    <input type="radio" name="DegreesIn" value="Fahrenheit">Fahrenheit<br>
-                    <input type="radio" name="DegreesIn" value="Kelvin">Kelvin<br></td>
-                <td><h3> to: </h3></td>
-                <td><input type="radio" name="DegreesOut" value="Celsius" checked="checked">Celsius<br>
-                    <input type="radio" name="DegreesOut" value="Fahrenheit">Fahrenheit<br>
-                    <input type="radio" name="DegreesOut" value="Kelvin">Kelvin<br></td>
-                <td><?php echo Convert($degreesIn, $degreesOut, $input); ?></td>
-            </tr>    
-            <tr>
-                <td id="button"><button type="submit" name="Convert" value="Convert">Convert</button>
-                    <button type="reset" value="Reset">Reset</button></td>
-            </tr>
-        </table>
-    </form>    
-</body>
-
 <?php
-    
-    function Convert($degreesIn, $degreesOut, $input){
+     function Convert($degreesIn, $degreesOut, $input){
         $output="";
         if ($degreesIn == "Celsius" && $degreesOut == "Celsius"){
             $output=$input;
-            echo $output;
+            return $output;
         }else if ($degreesIn == "Celsius" && $degreesOut == "Fahrenheit"){
             $output= $input*9/5+32;
             return $output;
@@ -78,11 +53,43 @@
         }
      }
     
-    
      if(isset($_POST["Convert"])){
-            $input =  $_POST['input'];
-            $degreesIn = $_POST['DegreesIn'];
-            $degreesOut = $_POST['DegreesOut'];
-            echo Convert($degreesIn, $degreesOut, $input);   
-     } 
+        $input =  $_POST['input'];     
+        $degreesIn = $_POST['DegreesIn'];
+        $degreesOut = $_POST['DegreesOut'];
+        $output = Convert($degreesIn, $degreesOut, $input);           
+    } 
 ?> 
+
+<body>
+    <h1>Temperature Converter</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+      <table>    
+            <tr>
+                <td><h3>Convert from:</h3></td>
+                <td><input type="text" name="input" size="6"></td>
+                <td><input type="radio" name="DegreesIn" value="Celsius" checked="checked">Celsius<br>
+                    <input type="radio" name="DegreesIn" value="Fahrenheit">Fahrenheit<br>
+                    <input type="radio" name="DegreesIn" value="Kelvin">Kelvin<br></td>
+                <td><h3> to: </h3></td>
+                <td><input type="radio" name="DegreesOut" value="Celsius" checked="checked">Celsius<br>
+                    <input type="radio" name="DegreesOut" value="Fahrenheit">Fahrenheit<br>
+                    <input type="radio" name="DegreesOut" value="Kelvin">Kelvin<br></td>
+            </tr>    
+            <tr>
+                <td id="button"><button type="submit" name="Convert" value="Convert">Convert</button></td>
+            </tr>
+        </table>
+        
+    </form>
+    <h3><?php
+            if (isset ($_POST['input']) && is_numeric($_POST['input'])){
+                echo $input . ' degrees ' . $degreesIn . ' is ' . $output . ' degrees ' . $degreesOut . '.';        
+            }
+            else {
+                echo "<font color=red>Please enter a number!</font>";
+            }
+        ?>
+    </h3>
+</body>
+
